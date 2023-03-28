94c7a957136a334a37d8f65ec36e76f782f49b3c
JHanslik <julien.hanslik@gmail.com>
2023-03-28 14:33:14 +0200

    connection WP gatsby done

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,29 +2,29 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `nabate`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        url: "nabate.local",
-      },
+    siteMetadata: {
+        title: `nabate`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-netlify",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-google-gtag",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    plugins: [
+        {
+            resolve: "gatsby-source-wordpress",
+            options: {
+                url: "http://nabate.local/graphql",
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-netlify",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-google-gtag",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
